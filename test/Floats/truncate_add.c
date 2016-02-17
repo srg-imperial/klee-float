ad0a9709459be0bdb733a24fbb7d8a1ed7cf7373
Dan Liew <daniel.liew@imperial.ac.uk>
2016-02-17 22:27:26 +0000

    Add two basic test cases for precise floating point constraints. These tests cases are taken from "Symbolic execution of Floating-point computatations".
    They currently fail because I haven't modified KLEE yet but the
    necessary changes will be coming soon. We also be able to adapt these
    test cases for testing the real approximation for floats in the future
    too.

---
+++ test/Floats/truncate_add.c
@@ -0,0 +1,26 @@
+// RUN: %llvmgcc %s -emit-llvm -O0 -g -c -o %t1.bc
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out --exit-on-error %t1.bc > %t-output.txt 2>&1
+// RUN: FileCheck -input-file=%t-output.txt %s
+#include "klee/klee.h"
+#include <stdio.h>
+
+// This based off an example from
+// "Symbolic execution of floating-point computations"
+// by Bernard Botella, Arnaud Gotlieb, and Claude Michel
+int main() {
+  float x, y, z;
+  klee_make_symbolic(&x, sizeof(float), "x");
+  klee_assume(x > 0.0); // This also implies x isn't a NaN
+  y = 1.0e12;
+  z = x + y;
+  if ( z == y ) {
+    // Note if we use reals to approximate floats this branch won't be feasible
+    printf("add x truncates\n");
+  } else {
+    printf("addition visible\n");
+  }
+  return 0;
+}
+// CHECK-NOT: silently concretizing (reason: floating point)
+// CHECK: KLEE: done: completed paths = 2
