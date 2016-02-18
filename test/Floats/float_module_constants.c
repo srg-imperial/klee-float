297c4be97da1f26d8e252ad1e1937fd181cbd5f1
Dan Liew <daniel.liew@imperial.ac.uk>
2016-02-18 22:46:15 +0000

    Implement basic support for a ``ConstantExpr`` to represent a floating point constant. The only way this is used right now is KModule constants that are of type ``llvm::ConstantFP`` are created using the ``APFloat`` constructor for ConstantExpr.
    A ``ConstantExpr`` that represent a float prints itself differently.
    Currently we support decimal and c99 hex float emission. When printing
    expressions we currently emit c99 hex float because there is never
    any precision loss. Unfortunately these are quite hard to read so I'm
    not sure if this is the right design decision.

---
+++ test/Floats/float_module_constants.c
@@ -0,0 +1,16 @@
+// RUN: %llvmgcc %s -emit-llvm -O0 -g -c -o %t1.bc
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out --exit-on-error %t1.bc > %t-output.txt 2>&1
+// RUN: FileCheck -input-file=%t-output.txt %s
+#include "klee/klee.h"
+
+int main() {
+  const float x = 0.5;
+  const double y = 0.75;
+  // CHECK: x:0x1p-1
+  klee_print_expr("x", x);
+  // CHECK: y:0x1.8p-1
+  klee_print_expr("y", y);
+  return 0;
+}
+// CHECK: KLEE: done: completed paths = 1
