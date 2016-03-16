d16bbb54e8e34d50ce7afcf2cfad24e74c34accb
Dan Liew <daniel.liew@imperial.ac.uk>
2016-03-16 14:44:40 +0000

    Fix FPToUIExpr test case and add another test case. I made a silly mistake in the first test case (the C99 spec says when converting a float to an int that it is truncated - i.e. rounded toward zero).

--- test/Floats/fptoui_simple2.c
+++ test/Floats/fptoui_simple2.c
@@ -10,12 +10,19 @@ int main() {
   float x;
   unsigned int y;
   klee_make_symbolic(&x, sizeof(float), "x");
-  klee_assume(x > 1.0f); // This also implies x isn't a NaN
+  klee_assume(x > 0.95); // This also implies x isn't a NaN
   klee_assume(x < 128.0f);
+  // The C11 spec (6.3.1.4) says this conversion should
+  // truncate the fractional part of ``x`` (i.e.
+  // this is like rounding toward 0).
   y = (unsigned int) x;
-  assert(y > 0);
+  if (y == 0) {
+    printf("y is zero\n");
+  } else {
+    printf("y non zero\n");
+  }
   assert(y < 128);
   return 0;
 }
 // CHECK-NOT: silently concretizing (reason: floating point)
-// CHECK: KLEE: done: completed paths = 1
+// CHECK: KLEE: done: completed paths = 2
