c91182cda0a286959b5a22387963a1c2f0d75855
Dan Liew <daniel.liew@imperial.ac.uk>
2016-04-28 00:05:04 +0100

    Implement FindArrayAckermannizationVisitor.
    The purpose of this visitor is to find reads of arrays that can
    ackermannized. The visitor doesn't actually make any modifications.
    It just looks for opportunities to do so. The current implementation
    is very conservative and will only say it can ackermannize simple
    contiguous reads of non-constant arrays without updates or simple
    array reads where the array has no updates. We can improve this
    in the future.
    
    We will use this to implement array ackermannization in the
    Z3Solver/Z3Builder.
    
    This not been tested an at all and is probably buggy...
    
    The use of ``goto`` is a little unfortunately but I can't
    think of an elegant and efficient way of handling failed matching
    without using C++11 lambdas.

---
+++ lib/Expr/FindArrayAckermannizationVisitor.h
@@ -0,0 +1,55 @@
+//===-- FindArrayAckermannizationVisitor.h ----------------------*- C++ -*-===//
+//
+//                     The KLEE Symbolic Virtual Machine
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef KLEE_FIND_ARRAY_ACKERMANNIZATION_VISITOR_H
+#define KLEE_FIND_ARRAY_ACKERMANNIZATION_VISITOR_H
+
+#include "klee/util/ExprVisitor.h"
+#include <map>
+
+namespace klee {
+
+struct ArrayAckermannizationInfo {
+  ref<Expr> toReplace;
+  bool isContiguousArrayRead();
+  const Array *getArray();
+};
+
+/// This class looks for opportunities to perform ackermannization (ackermann's
+/// reduction) of array reads.  The reduction here is transforming array reads
+/// into uses of bitvector variables of bitwidth <= ``maxArrayWidth``. Note
+/// this visitor doesn't actually modify the expressions given to it, instead
+/// it just looks for opportunities to apply the reduction.
+class FindArrayAckermannizationVisitor : ExprVisitor {
+public:
+  FindArrayAckermannizationVisitor(bool recursive, unsigned maxArrayWidth);
+  void clear();
+  const unsigned maxArrayWidth; // maxWidth of Arrays in bits to consider as
+                                // candidates for ackermannization
+
+  // FIXME: Should we hide this behind an interface?
+  /// The recorded Array's that can be ackermannized.
+  /// If an Array maps to empty vector then the detected array cannot be
+  /// ackermannized.
+  /// If an Array maps to a non-empty vector then that vector gives all the
+  /// expressions
+  /// that can be ackermannized.
+  typedef std::map<const Array *, std::vector<ArrayAckermannizationInfo> >
+      ArrayToAckermannizationInfoMapTy;
+  ArrayToAckermannizationInfoMapTy ackermannizationInfo;
+  std::vector<ArrayAckermannizationInfo> *
+  getOrInsertAckermannizationInfo(const Array *, bool *wasInsert);
+
+protected:
+  Action visitConcat(const ConcatExpr &);
+  Action visitRead(const ReadExpr &);
+};
+}
+
+#endif
