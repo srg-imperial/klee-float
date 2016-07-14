3268b2298516460189127f64eec01f6149780cf7
Rafael Zaehl <rafael.zaehl@rwth-aachen.de>
2016-07-14 17:38:54 +0200

    copy/paste mistake

--- lib/Core/SpecialFunctionHandler.cpp
+++ lib/Core/SpecialFunctionHandler.cpp
@@ -835,7 +835,7 @@ void SpecialFunctionHandler::handleFeGetExceptFlag(ExecutionState &state,
     os->write8(i, flagArr[i]);
   }
 
-  ref<ConstantExpr> retExpr = ConstantExpr::alloc(0, sizeof(int) * 8);
+  ref<ConstantExpr> retExpr = ConstantExpr::alloc(ret, sizeof(ret) * 8);
   executor.bindLocal(target, state, retExpr);
 }
 
@@ -879,7 +879,7 @@ void SpecialFunctionHandler::handleFeSetExceptFlag(ExecutionState &state,
     os->write8(i, flagArr[i]);
   }
 
-  ref<ConstantExpr> retExpr = ConstantExpr::alloc(0, sizeof(int) * 8);
+  ref<ConstantExpr> retExpr = ConstantExpr::alloc(ret, sizeof(ret) * 8);
   executor.bindLocal(target, state, retExpr);
 }
 
@@ -963,7 +963,7 @@ void SpecialFunctionHandler::handleFeHoldExcept(ExecutionState &state,
     os->write8(i, envArr[i]);
   }
 
-  ref<ConstantExpr> retExpr = ConstantExpr::alloc(0, sizeof(int) * 8);
+  ref<ConstantExpr> retExpr = ConstantExpr::alloc(ret, sizeof(ret) * 8);
   executor.bindLocal(target, state, retExpr);
 }
 
@@ -1019,6 +1019,6 @@ void SpecialFunctionHandler::handleFeUpdateEnv(ExecutionState &state,
     os->write8(i, envArr[i]);
   }
 
-  ref<ConstantExpr> retExpr = ConstantExpr::alloc(0, sizeof(int) * 8);
+  ref<ConstantExpr> retExpr = ConstantExpr::alloc(ret, sizeof(ret) * 8);
   executor.bindLocal(target, state, retExpr);
 }
