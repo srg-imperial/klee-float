a3fee6c42885642c887f00d0c4cf81018b3252d4
Dan Liew <daniel.liew@imperial.ac.uk>
2016-11-25 10:44:55 +0000

    Add test broken test cases that test the forking behaviour of `isinf()`.
    We don't want it to fork so we can use it in `klee_assume()` but
    the current implementation forks. This needs to be fixed.

---
+++ test/Floats/isinf_double_no_fork.c
@@ -0,0 +1,20 @@
+// RUN: %llvmgcc %s -emit-llvm -O0 -g -c -o %t1.bc
+// RUN: rm -rf %t.klee-out
+// RUN: %klee --output-dir=%t.klee-out %t1.bc > %t-output.txt 2>&1
+// RUN: FileCheck -input-file=%t-output.txt %s
+
+// FIXME: KLEE needs to be fixed to make this test pass
+// XFAIL: *
+#include "klee/klee.h"
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+
+int main() {
+  double x;
+  klee_make_symbolic(&x, sizeof(double), "x");
+  klee_assume(isinf(x));
+  assert(klee_is_infinite_double(x));
+}
+// CHECK-NOT: invalid klee_assume call (provably false)
+// CHECK: KLEE: done: completed paths = 1
