e2916ed60909e61cee3e8e030fa30f2cb6dbca07
Dan Liew <daniel.liew@imperial.ac.uk>
2016-12-01 19:09:01 +0000

    Fix test case under ASan build. It seems using ASan influences the order the bug is found leading to a different test case number to be used for the expected bug. Using DFS as the search seems to fix this.

--- test/regression/2014-07-04-unflushed-error-report.c
+++ test/regression/2014-07-04-unflushed-error-report.c
@@ -1,6 +1,6 @@
 // RUN: %llvmgcc %s -emit-llvm -g -O0 -c -o %t.bc
 // RUN: rm -rf %t.klee-out
-// RUN: %klee --output-dir=%t.klee-out -check-overshift %t.bc 2> %t.log
+// RUN: %klee --search=dfs --output-dir=%t.klee-out -check-overshift %t.bc 2> %t.log
 // RUN: FileCheck -input-file=%t.klee-out/test000001.overshift.err %s
 
 /* This test checks that the error file isn't empty and contains the
