5652c803a2936fc1d44b663441601bacc120bb92
Dan Liew <daniel.liew@imperial.ac.uk>
2016-12-06 11:19:02 +0000

    Improve comments on clang crashing source.

--- utils/hacks/fp_test/crash_clang34.cpp
+++ utils/hacks/fp_test/crash_clang34.cpp
@@ -3,6 +3,14 @@
 #include <stdio.h>
 
 int main() {
+  // These special constants are part of the "Positive Floating point
+  // Unnormals" as defined in 8.2.2 "Unsupported Double Extended-Precision
+  // Floating-Point Encodings and Pseduo-Denormals" of the Intel(R) 64 and IA-32
+  // Architectures Software Developer's Manual.
+  //
+  //                              Significand
+  // Sign Exponent             Integer Fraction
+  // [0]  [111 1111 1111 1110] [1]     [ 63 ones ]
   const uint64_t lowBits = 0x7fffffffffffffff;
   const uint16_t highBits = 0x7ffe;
   long double v = 0.0l;
