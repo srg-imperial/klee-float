eb303ed7b22586afaa32f623d91d75a41d1a4730
Dan Liew <daniel.liew@imperial.ac.uk>
2016-12-09 11:22:55 +0000

    Fix the size used for allocating memory for stack variables and globals. KLEE was using `getTypeStoreSize()` which doesn't include padding which is not zero for some types (e.g. `long double` on x86_64). Previously this meant we would report out of bounds access when accessing bytes would exist during native execution due to alignment.
    This means we don't need to hack in
    `SpecialFunctionHandler::handleMakeSymbolic` to examine alignment
    as the right amount of memory is now allocated for MemoryObjects.

--- lib/Core/SpecialFunctionHandler.cpp
+++ lib/Core/SpecialFunctionHandler.cpp
@@ -759,24 +759,7 @@ void SpecialFunctionHandler::handleMakeSymbolic(ExecutionState &state,
                                   res);
     assert(success && "FIXME: Unhandled solver failure");
 
-    bool isAlignedSize = false;
-    if (!res && mo->alignment != 0 && mo->alignment > mo->size) {
-      // klee_make_symboic might be passed `sizeof(long double)`.
-      // On x86_64 that is 16 even though the `long double` is actually
-      // 10 bytes. This can lead the above solver query to fail.
-
-      // Try the query again to see if the alignment is the size requested.
-      ref<ConstantExpr> alignmentExpr =
-          ConstantExpr::create(mo->alignment, Context::get().getPointerWidth());
-      bool success __attribute__((unused)) = executor.solver->mustBeTrue(
-          *s, EqExpr::create(ZExtExpr::create(arguments[1],
-                                              Context::get().getPointerWidth()),
-                             alignmentExpr),
-          isAlignedSize);
-      assert(success && "FIXME: Unhandled solver failure");
-    }
-
-    if (res || isAlignedSize) {
+    if (res) {
       executor.executeMakeSymbolic(*s, mo, name);
     } else {      
       executor.terminateStateOnError(*s, 
