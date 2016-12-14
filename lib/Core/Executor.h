3a0515955cc8568a1fcdc223087b1b75a16cf886
Dan Liew <daniel.liew@imperial.ac.uk>
2016-12-14 14:38:39 +0000

    [REWORK of upstream patch] Sketch the fix that needs to happen in KLEE to handle loading from padded types (e.g. `long double`). Unfortunately I've had to disable this because KLEE contains implicit assumptions that the size of a type (padded and not padded) and the size of an Expr are all the same (e.g. FCmp assumes that the width of the Expr can be used to determine the floating point semantics).

--- lib/Core/Executor.h
+++ lib/Core/Executor.h
@@ -508,7 +508,7 @@ public:
   virtual void getCoveredLines(const ExecutionState &state,
                                std::map<const std::string*, std::set<unsigned> > &res);
 
-  Expr::Width getWidthForLLVMType(LLVM_TYPE_Q llvm::Type *type) const;
+  Expr::Width getWidthForLLVMType(LLVM_TYPE_Q llvm::Type *type, bool includePadding=false) const;
   size_t getAllocationAlignment(const llvm::Value *allocSite) const;
 
   // Return the halt timer if it exists.
