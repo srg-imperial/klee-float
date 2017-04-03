2e1232376931358fd3eb5e3a83316aa0fc40091c
Dan Liew <daniel.liew@imperial.ac.uk>
2017-04-03 14:05:01 +0100

    Add `-use-query-log=solver:core_aa` option so queries can be logged with ackermannization performed to them.
    This allows us to do `-use-query-log=solver:core,solver:core_aa`
    which will allow us to log the ackermannized and non-ackermannized
    versions of queries at the same time. We need this to get benchmarks
    for SMT-COMP.

--- include/klee/CommandLine.h
+++ include/klee/CommandLine.h
@@ -36,13 +36,15 @@ enum QueryLoggingSolverType {
   ALL_PC,     ///< Log all queries (un-optimised) in .pc (KQuery) format
   ALL_SMTLIB, ///< Log all queries (un-optimised)  .smt2 (SMT-LIBv2) format
   ALL_CORE_SOLVER_LANG, ///< Log all queries (un-optimised) in core solver's
-                        ///native format
+                        /// native format
   SOLVER_PC,     ///< Log queries passed to solver (optimised) in .pc (KQuery)
-                 ///format
+                 /// format
   SOLVER_SMTLIB, ///< Log queries passed to solver (optimised) in .smt2
                  ///(SMT-LIBv2) format
-  SOLVER_CORE_SOLVER_LANG ///< Log queries passed to solver in core solver's
-                          ///native format
+  SOLVER_CORE_SOLVER_LANG,   ///< Log queries passed to solver in core solver's
+                             /// native format
+  SOLVER_CORE_SOLVER_LANG_AA ///< Log queries passed to solver in core solver's
+  /// native format with array ackermannization if supported.
 };
 
 /* Using cl::list<> instead of cl::bits<> results in quite a bit of ugliness when it comes to checking
--- lib/Basic/ConstructSolverChain.cpp
+++ lib/Basic/ConstructSolverChain.cpp
@@ -10,8 +10,9 @@
 /*
  * This file groups declarations that are common to both KLEE and Kleaver.
  */
-#include "klee/Common.h"
 #include "klee/CommandLine.h"
+#include "klee/Common.h"
+#include "klee/ConstraintLogConfig.h"
 #include "klee/Constraints.h"
 #include "klee/Internal/Support/ErrorHandling.h"
 #include "llvm/Support/raw_ostream.h"
@@ -57,6 +58,36 @@ Solver *constructSolverChain(Solver *coreSolver, std::string querySMT2LogPath,
     klee_message(
         "Logging queries that reach solver in core solver's language to %s\n",
         filePath.c_str());
+  }
+
+  if (optionIsSet(queryLoggingOptions, SOLVER_CORE_SOLVER_LANG_AA)) {
+    const char *fileExtension = NULL;
+
+    // FIXME: Kind of gross. Should probably have method on solver
+    // that just gives us the file extension.
+    // Create dummy query to determine file extension
+    ConstraintManager cm;
+    Query q(cm, ConstantExpr::alloc(0, Expr::Bool));
+    char *dummy = solver->getConstraintLog(q, &fileExtension, /*clc=*/NULL);
+    free(dummy);
+    std::string filePath = baseCoreSolverLangLogPath;
+    filePath += "aa."; // give different name
+    filePath += fileExtension;
+
+    ConstraintLogConfig *clc = NULL;
+    Z3ConstraintLogConfig z3clc;
+    z3clc.ackermannizeArrays = true;
+    if (CoreSolverToUse == Z3_SOLVER) {
+      clc = &z3clc;
+    } else {
+      llvm::errs() << "Core solver is not Z3, cannot ackermannize arrays\n";
+    }
+
+    solver = createCoreSolverLangLoggingSolver(solver, filePath,
+                                               MinQueryTimeToLog, /*clc=*/clc);
+    klee_message("Logging all (might be ackermannized) queries in core "
+                 "solver's language to %s\n",
+                 filePath.c_str());
   }
 
   if (UseAssignmentValidatingSolver)
