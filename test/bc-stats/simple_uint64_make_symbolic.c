3b944725408e31e88e4097aace27c80192d4470e
Dan Liew <daniel.liew@imperial.ac.uk>
2017-05-08 19:32:27 +0100

    Teach the bc-stats tool to estimate the number of symbolic bytes in a program. The approach is incredibly naive but it'll do for now.

---
+++ test/bc-stats/simple_uint64_make_symbolic.c
@@ -0,0 +1,19 @@
+// RUN: %llvmgcc %s -emit-llvm -O0 -c -o %t1.bc
+// RUN: %bc-stats %t1.bc | FileCheck %s
+#include "klee/klee.h"
+#include <stdio.h>
+
+
+int main(int argc, char** argv) {
+  uint64_t a;
+  klee_make_symbolic(&a, sizeof(a), "a");
+  // CHECK-DAG: num_branches: 1
+  // CHECK-DAG: num_function_defns: 1
+  // CHECK-DAG: estimated_num_symbolic_bytes: 8
+  if (argc) {
+    printf("argc\n");
+  } else {
+    printf("!argc\n");
+  }
+  return 0;
+}
--- tools/bc-stats/main.cpp
+++ tools/bc-stats/main.cpp
@@ -12,10 +12,12 @@
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
 #include "llvm/PassManager.h"
+#include "llvm/Support/CallSite.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/IPO.h"
+#include <llvm/IR/Constants.h>
 #include <string>
 #include <vector>
 
@@ -29,33 +31,135 @@ cl::opt<std::string>
     EntryPoint("entry-point",
                cl::desc("Entry Point to use for DCE. By default don't do DCE"),
                cl::init(""));
+
+cl::opt<bool> OnlyCountSymbolicBytesInEntryPoint(
+    "only-symbolic-bytes-in-entry",
+    cl::desc("Only count symbolic bytes in the entry point functions (default: "
+             "false)"),
+    cl::init(false));
 }
 
 enum ExitCodes {
   SUCCESS,
   FAIL_TO_OPEN = 1,
   FAIL_TO_PARSE = 2,
-  FAIL_TO_FIND_ENTRY_POINT = 3
+  FAIL_TO_FIND_ENTRY_POINT = 3,
+  INVALID_CMD_LINE_OPTIONS = 4
 };
 
 struct BCSTats {
-  uint64_t num_function_defns; // Function definitions
-  uint64_t num_function_decls; // Function declarations
-  uint64_t num_branches;       // Number of branches in module
+  uint64_t num_function_defns;           // Function definitions
+  uint64_t num_function_decls;           // Function declarations
+  uint64_t num_branches;                 // Number of branches in module
+  uint64_t estimated_num_symbolic_bytes; // Estimated number of symbolic bytes
 
-  BCSTats() : num_function_defns(0), num_function_decls(0), num_branches(0){};
+  BCSTats()
+      : num_function_defns(0), num_function_decls(0), num_branches(0),
+        estimated_num_symbolic_bytes(0){};
 
   void dump(llvm::raw_ostream &os) {
+    // Output as YAML
     os << "num_branches: " << num_branches
        << "\nnum_function_defns: " << num_function_defns
-       << "\nnum_function_decls: " << num_function_decls << "\n";
+       << "\nnum_function_decls: " << num_function_decls
+       << "\nestimated_num_symbolic_bytes: " << estimated_num_symbolic_bytes
+       << "\n";
   }
 
   void dump() { dump(errs()); }
 };
 
+// FIXME: This is very naive. In the presence of any branching control flow
+// this estimate is wrong.
+// FIXME: Only count `klee_make_symbolic` calls in BBs that dominate every exit
+// BB. That way we only count klee_make_symbolic calls that have to be exited
+// in the function.
+void countSymbolicBytes(const Function *func, BCSTats &moduleStats) {
+  uint64_t symbolic_bytes_requested_in_function = 0;
+  if (OnlyCountSymbolicBytesInEntryPoint) {
+    if (func->getName() != EntryPoint) {
+      KLEE_DEBUG_WITH_TYPE("symbolic_count",
+                           errs() << "Skipping non-entry point function \""
+                                  << func->getName() << "\"\n");
+    }
+  }
+  for (Function::const_iterator BB = func->begin(), BE = func->end(); BB != BE;
+       ++BB) {
+    for (BasicBlock::const_iterator ii = BB->begin(), ie = BB->end(); ii != ie;
+         ++ii) {
+      // FIXME: Should use `ImmutableCallSite`. Can't use it like `CallSite`
+      // in LLVM 3.4
+      CallSite cs;
+      if (const CallInst *ci = dyn_cast<CallInst>(ii)) {
+        cs = CallSite(const_cast<CallInst *>(ci));
+      } else if (const InvokeInst *invInst = dyn_cast<InvokeInst>(ii)) {
+        cs = CallSite(const_cast<InvokeInst *>(invInst));
+      }
+      if (!cs)
+        continue;
+      Function *callee = cs.getCalledFunction();
+      if (!callee || callee->getName() != "klee_make_symbolic")
+        continue;
+
+      assert((cs.arg_size() == 2 || cs.arg_size() == 3) &&
+             "wrong number of args");
+      Value *bytes = cs.getArgument(1);
+      if (const ConstantInt *CE = dyn_cast<ConstantInt>(bytes)) {
+        assert(CE->getValue().getBitWidth() <= 64 && "Too wide");
+        symbolic_bytes_requested_in_function +=
+            CE->getValue().getLimitedValue();
+      } else {
+        errs() << "WARNING: Could not handle argument to klee_make_symbolic: "
+               << *bytes << "\n";
+      }
+    }
+  }
+  KLEE_DEBUG_WITH_TYPE(
+      "symbolic_count",
+      errs() << "Estimated that \"" << func->getName() << "\" requested "
+             << symbolic_bytes_requested_in_function << " bytes\n");
+  moduleStats.estimated_num_symbolic_bytes +=
+      symbolic_bytes_requested_in_function;
+}
+
+void countBranches(const Function *func, BCSTats &moduleStats) {
+  uint64_t num_branches_in_function = 0;
+  for (Function::const_iterator BB = func->begin(), BE = func->end(); BB != BE;
+       ++BB) {
+    for (BasicBlock::const_iterator ii = BB->begin(), ie = BB->end(); ii != ie;
+         ++ii) {
+      // This covers BranchInst, SwitchInst, IndirectBrInst, ReturnInst, etc..
+      if (const TerminatorInst *ti = dyn_cast<TerminatorInst>(ii)) {
+        KLEE_DEBUG_WITH_TYPE("branching",
+                             errs() << "Found terminator instruction\n");
+        KLEE_DEBUG_WITH_TYPE("branching", ti->dump());
+        uint64_t successors = ti->getNumSuccessors();
+        if (successors) {
+          num_branches_in_function += (successors - 1);
+          KLEE_DEBUG_WITH_TYPE("branching",
+                               errs() << "successors: " << successors << "\n");
+          if (successors > 1)
+            KLEE_DEBUG_WITH_TYPE("branching",
+                                 errs() << "Adding " << (successors - 1)
+                                        << " branches\n");
+        }
+      }
+    }
+  }
+  KLEE_DEBUG_WITH_TYPE("branching",
+                       errs() << "Found " << num_branches_in_function
+                              << " branches in " << func->getName() << "\n");
+  moduleStats.num_branches += num_branches_in_function;
+}
+
 int main(int argc, char **argv) {
   cl::ParseCommandLineOptions(argc, argv, " bc-stats");
+
+  if (OnlyCountSymbolicBytesInEntryPoint && EntryPoint.size() == 0) {
+    errs() << "Entry point must be specified when using "
+              "-only-symbolic-bytes-in-entry\n";
+    return INVALID_CMD_LINE_OPTIONS;
+  }
 
   LLVMContext ctx;
   Module *m = NULL;
@@ -91,42 +195,16 @@ int main(int argc, char **argv) {
   BCSTats moduleStats;
 
   // Walk through functions
-  for (Module::iterator func = m->begin(), fie = m->end(); func != fie;
+  for (Module::const_iterator func = m->begin(), fie = m->end(); func != fie;
        ++func) {
     if (func->isDeclaration()) {
       ++moduleStats.num_function_decls;
       continue;
     }
     ++moduleStats.num_function_defns;
-    errs() << "Processing: " << func->getName() << "\n";
-
-    // TODO: Implement algorithm to count number of symbolic bytes.
-    uint64_t num_branches_in_function = 0;
-    for (Function::iterator BB = func->begin(), BE = func->end(); BB != BE;
-         ++BB) {
-      for (BasicBlock::iterator ii = BB->begin(), ie = BB->end(); ii != ie;
-           ++ii) {
-        // This covers BranchInst, SwitchInst, IndirectBrInst, ReturnInst, etc..
-        if (TerminatorInst *ti = dyn_cast<TerminatorInst>(ii)) {
-          KLEE_DEBUG_WITH_TYPE("branching",
-                               errs() << "Found terminator instruction\n");
-          KLEE_DEBUG_WITH_TYPE("branching", ti->dump());
-          uint64_t successors = ti->getNumSuccessors();
-          if (successors) {
-            num_branches_in_function += (successors - 1);
-            KLEE_DEBUG_WITH_TYPE(
-                "branching", errs() << "successors: " << successors << "\n");
-            if (successors > 1)
-              KLEE_DEBUG_WITH_TYPE("branching",
-                                   errs() << "Adding " << (successors - 1)
-                                          << " branches\n");
-          }
-        }
-      }
-    }
-    errs() << "Found " << num_branches_in_function << " branches in "
-           << func->getName() << "\n";
-    moduleStats.num_branches += num_branches_in_function;
+    // errs() << "Processing: " << func->getName() << "\n";
+    countBranches(func, moduleStats);
+    countSymbolicBytes(func, moduleStats);
   }
 
   // Dump stats
